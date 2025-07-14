f5e066fc2313126f5e53c1f8fd33505e008eca7b
50101063 <gokuls1@ltfs.com>
2025-07-14 18:39:48 +0530

    feat: create root App component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>V-KYC Recordings Audit Portal</h1>
+      </header>
+      <main>
+        {/* App content will go here */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
